83dfc2282f5e6456220446c69d6e1bf4c5cfdb1b
git <2959077377@qq.com>
2016-04-16 18:07:06 +0800

    initail project

---
+++ LearningFFmpeg/LearningFFmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearningFFmpeg
+//
+//  Created by ZhangLe on 16/4/16.
+//  Copyright © 2016年 30days-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearningFFmpeg/LearningFFmpeg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearningFFmpeg
+//
+//  Created by ZhangLe on 16/4/16.
+//  Copyright © 2016年 30days-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
